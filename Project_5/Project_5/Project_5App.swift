92b4fb55dd294108a25bd31f51afb499c2ccf2a3
Lena <es6849633@gmail.com>
2024-11-20 20:59:06 +0300

    first commit

---
+++ Project_5/Project_5/Project_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_5App.swift
+//  Project_5
+//
+//  Created by Елена on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Project_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project_5/Project_5Tests/Project_5Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Project_5Tests.swift
+//  Project_5Tests
+//
+//  Created by Елена on 14.11.2024.
+//
+
+import Testing
+
+struct Project_5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
